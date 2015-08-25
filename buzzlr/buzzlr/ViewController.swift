4b539ad05a78f4e38e3ca262d253c3272e0ff4b6
Dulio Denis <dulio.denis@yahoo.com>
2015-08-25 14:20:56 -0400

    Added URL Scheme

--- buzzlr/buzzlr/ViewController.swift
+++ buzzlr/buzzlr/ViewController.swift
@@ -34,10 +34,11 @@ class ViewController: UIViewController {
                 forUserAccount: OAUTH, inService: BUZZLR)
             
             self.showAlertView("Tumblr", message: "oauth_token:\(credential.oauth_token)\n\noauth_toke_secret:\(credential.oauth_token_secret)")
+            self.getTumblrData()
             }, failure: {(error:NSError!) -> Void in
                 println(error.localizedDescription)
         })
-        
+/*
         let OAUTH = "OAuthData"
         let BUZZLR = "buzzlr"
         // Load from the Keychain
@@ -50,14 +51,38 @@ class ViewController: UIViewController {
             // Locksmith couldn’t find anything in the keychain
             self.showAlertView("Token", message: "No token found.")
         }
-        
+*/
     }
-    
+
     func showAlertView(title: String, message: String) {
         var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.Default, handler: nil))
         self.presentViewController(alert, animated: true, completion: nil)
     }
 
+    
+    func getTumblrData() {
+        let TAG = "BusinessMen"
+        let API_KEY = "CC3BXFyI4YwGviO2AZk3fM1jgHdaa6WtKO7x9YxbAjNvqvqfhl"
+        let urlPath = NSURL(string: "https://api.tumblr.com/v2/tagged?tag=\(TAG)&api_key=\(API_KEY)")
+        
+        var session = NSURLSession.sharedSession()
+        var task = session.dataTaskWithURL(urlPath!) {
+            data, response, error -> Void in
+            
+            if ((error) != nil) {
+                print(error.localizedDescription)
+            }
+            
+            var jsonError : NSError?
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as! Dictionary<String, String>
+            
+            if ((jsonError) != nil) {
+                print(jsonError!.localizedDescription)
+            } else {
+                print(jsonResult)
+            }
+        }
+    }
 }
 
